65e82cdf826b399ebc128a05bd170cb5d95556ad
Chufretalas <marcobene.contact@gmail.com>
2022-12-03 14:29:02 -0300

    first challenge done

---
+++ day1-c1/day1.py
@@ -0,0 +1,22 @@
+def main():
+    biggest_calories = 0
+    calories = 0
+    with open("./input.txt", "r") as file:
+        while True:
+            line = file.readline()
+            if line:
+                if line == "\n":
+                    if calories > biggest_calories: biggest_calories = calories
+                    calories = 0
+                else:
+                    calories += int(line.removesuffix("\n"))
+            else:
+                break
+
+        print(biggest_calories)
+
+        file.close()
+
+
+if __name__ == "__main__":
+    main()
