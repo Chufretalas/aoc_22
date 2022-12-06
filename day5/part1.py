5d7377841df5446c8769ddf449f929a322b4d091
Chufretalas <marcobene.contact@gmail.com>
2022-12-05 23:00:06 -0300

    added some comments to the partial d5p1 solution

--- day5/part1.py
+++ day5/part1.py
@@ -2,34 +2,32 @@ from os import path
 
 
 def parse_stacks(stacks_raw: list[str]):
-    stacks = [[], [], [], [], [], [], [], [], []]
+    stacks = [[], [], [], [], [], [], [], [], []] # saves the stacks from 0 to 8 (1 - 9 in the instructions)
     for line in stacks_raw:
-        start_index = 0
-        stack_to_fill = 0
-        end_line = False
-        while True:
-            if start_index >= len(line):
-                break
-            index = 0
+        start_index = 0  # starting for searching "["
+        stack_to_fill = 0 # wich stack it is supposed to fill with content
+        end_line = False # if the contents from the line are done
+        while True: # loops the process of finds "[" -> fill the gaps -> fill the letter
+            index = 0 # declares the index where "[" is found
             try:
                 index = line[start_index:].index("[")
-            except:
-                for _ in range(9-stack_to_fill):
+            except: # if none "[" is found, it means that there's no more letters in the line
+                for _ in range(9-stack_to_fill): # fills the remaining spaces with "-" (blanks)
                     stacks[stack_to_fill].append("-")
                     stack_to_fill += 1
                     end_line = True
             if end_line:
                 break
-            empty_stacks = index/4
-            if not (empty_stacks < 1):
+            empty_stacks = index/4 # finds how many stacks are missing from the last spot searched
+            if not (empty_stacks < 1): # if there's no missing stack, then the spaces (1) divided by 4 is lesser than one (0.25)
                 empty_stacks = int(empty_stacks)
-                for _ in range(empty_stacks):
+                for _ in range(empty_stacks): # fills the empty spaces
                     stacks[stack_to_fill].append("-")
                     stack_to_fill += 1
             if stack_to_fill > 8: break
-            stacks[stack_to_fill].append(line[index+start_index+1])
+            stacks[stack_to_fill].append(line[index+start_index+1]) # fills the letter that was found in the beginning with the "[" 
             stack_to_fill += 1
-            start_index = start_index + index + 2
+            start_index = start_index + index + 2 # updates the start index so the next search starts from the "]"
 
     print(stacks)
 
